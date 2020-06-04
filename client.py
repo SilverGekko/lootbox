b3e4a5e522626139fcd6e162c599a56352b3f2e8
Nick Pugliese <nicholas.d.pugliese@gmail.com>
2020-06-03 20:56:45 -0700

    update indentation to 2 spaces

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-#/bin/python3
+# /bin/python3
 
 import tkinter as tk
 from tkinter import ttk
@@ -15,134 +15,139 @@ BUFFER_SIZE = 1024
 MESSAGE = b"Message in a bottle"
 DIR = "src_files/"
 
-def list_dir_files(path):  
-    for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)):
-            yield file
+
+def list_dir_files(path):
+  for file in os.listdir(path):
+    if os.path.isfile(os.path.join(path, file)):
+      yield file
+
 
 class ClientApp(tk.Tk):
-    def __init__(self):
-        super().__init__()
+  def __init__(self):
+    super().__init__()
 
-        if not os.path.isdir(DIR):
-            os.mkdir(DIR)
+    if not os.path.isdir(DIR):
+      os.mkdir(DIR)
 
-        self.connection_data = self.load_connection_data()
+    self.connection_data = self.load_connection_data()
 
-        self.title("Lootbox Client")
+    self.title("Lootbox Client")
 
-        self.resizable(False, False)
+    self.resizable(False, False)
 
-        # center the window
-        # assuming you have a symmetrical setup of monitors...
-        window_width = self.winfo_reqwidth()
-        window_height = self.winfo_reqwidth()
+    # center the window
+    # assuming you have a symmetrical setup of monitors...
+    window_width = self.winfo_reqwidth()
+    window_height = self.winfo_reqwidth()
 
-        position_right = int(self.winfo_screenwidth() / 2 - window_width / 2)
-        position_down = int(self.winfo_screenheight() / 3 - window_height / 2)
+    position_right = int(self.winfo_screenwidth() / 2 - window_width / 2)
+    position_down = int(self.winfo_screenheight() / 3 - window_height / 2)
 
-        self.geometry("+{}+{}".format(position_right, position_down))
-        ###########################################################################################
-        # buttons
-        ###########################################################################################
+    self.geometry("+{}+{}".format(position_right, position_down))
+    ###########################################################################################
+    # buttons
+    ###########################################################################################
 
-        self.file_list = tk.Listbox(self, selectmode='extended')
-        self.file_list.pack()
-        # for file in files("./src_files"):
-        #     self.file_list.insert(tk.END, file)
-        self.refresh_list(self.file_list)
-        
-        self.send_file_button = tk.Button(text="Send File", command=lambda: self.send_file(self.file_list.get(self.file_list.curselection()), self.connection_data) )
-        self.send_file_button.pack()
+    self.file_list = tk.Listbox(self, selectmode='extended')
+    self.file_list.pack()
+    # for file in files("./src_files"):
+    #   self.file_list.insert(tk.END, file)
+    self.refresh_list(self.file_list)
 
-        self.refresh_button = tk.Button(text="Refresh List", command=lambda: self.refresh_list(self.file_list) )
-        self.refresh_button.pack()
+    self.send_file_button = tk.Button(text="Send File", command=lambda: self.send_file(
+      self.file_list.get(self.file_list.curselection()), self.connection_data))
+    self.send_file_button.pack()
 
-        self.server_connection_light = tk.Label(background='green4')
-        self.server_connection_light.pack(fill=tk.X)
+    self.refresh_button = tk.Button(
+      text="Refresh List", command=lambda: self.refresh_list(self.file_list))
+    self.refresh_button.pack()
 
-    def ping_server(self, connection_data):
-        # big massive TODO:
-        # think about what makes sense:
-        #   - having the client connected to the server all the time
-        #   - having a connection be made for each file sent (current implementation)
-        #   - having the client connect and stay connected until a timeout happens, then diconnect regardless (good)
-        #   - having the client connect and stay connected until a timeout happens, then diconnect if no activity (good)
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            try:
-                s.connect((connection_data["host"], connection_data["port"]))
-            except ConnectionRefusedError:
-                print("Could not establish a connection to the server at", connection_data)
-                return 1
-            return 0
+    self.server_connection_light = tk.Label(background='green4')
+    self.server_connection_light.pack(fill=tk.X)
 
-    def connection_light(self, light):
-        
-        self.after(2000, self.connection_light, light)
+  def ping_server(self, connection_data):
+    # big massive TODO:
+    # think about what makes sense:
+    #   - having the client connected to the server all the time
+    #   - having a connection be made for each file sent (current implementation)
+    #   - having the client connect and stay connected until a timeout happens, then diconnect regardless (good)
+    #   - having the client connect and stay connected until a timeout happens, then diconnect if no activity (good)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      try:
+        s.connect((connection_data["host"], connection_data["port"]))
+      except ConnectionRefusedError:
+        print("Could not establish a connection to the server at", connection_data)
+        return 1
+      return 0
 
-    def load_connection_data(self):
-        """
-        Returns a dict full of the connection data from the parsed json config file.
+  def connection_light(self, light):
+    self.after(2000, self.connection_light, light)
 
-        If no file named 'connection.json' exists, it will be initialized with default values and created.
-        """
-        try:
-            json_data = open('connection.json', 'r')
-        except FileNotFoundError:
-            json_data = open('connection.json', 'w+')
-            data_dict = {
-                "name" : "local server",
-                "host" : "127.0.0.1",
-                "port" : 5003,
-            }
-            json.dump(data_dict, json_data)
-            json_data.seek(0) # go back to the beginning since we just wrote a bunch of data
-        connection_data = json.loads(json_data.read())
-        return connection_data
+  def load_connection_data(self):
+    """
+    Returns a dict full of the connection data from the parsed json config file.
 
-    def refresh_list(self, listbox):
-        listbox.delete(0, tk.END)
-        for file in list_dir_files("./src_files"):
-            listbox.insert(tk.END, file)
+    If no file named 'connection.json' exists, it will be initialized with default values and created.
+    """
+    try:
+      json_data = open('connection.json', 'r')
+    except FileNotFoundError:
+      json_data = open('connection.json', 'w+')
+      data_dict = {
+          "name": "local server",
+          "host": "127.0.0.1",
+          "port": 5003,
+      }
+      json.dump(data_dict, json_data)
+      # go back to the beginning since we just wrote a bunch of data
+      json_data.seek(0)
+    connection_data = json.loads(json_data.read())
+    return connection_data
 
-    def send_file(self, filename, connection_data):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            try:
-                s.connect((connection_data["host"], connection_data["port"]))
-            except ConnectionRefusedError:
-                print("Could not establish a connection to the server at", connection_data)
-                return
+  def refresh_list(self, listbox):
+    listbox.delete(0, tk.END)
+    for file in list_dir_files("./src_files"):
+      listbox.insert(tk.END, file)
 
-            #os.listdir("files")
-            #filename = input()
+  def send_file(self, filename, connection_data):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      try:
+        s.connect((connection_data["host"], connection_data["port"]))
+      except ConnectionRefusedError:
+        print("Could not establish a connection to the server at", connection_data)
+        return
 
-            f = open(DIR + filename, "rb") #r - open for reading, b - binary mode
-            #s.send(bytes(filename, 'utf-8') + (b'\0' *  (64 - len(filename) ))) # send over the filename json so the server knows what we want to send
-            json_to_send = utils.jsonify(DIR + filename)
-            # max json data size of 1024 bytes
-            # send the server the json data of the file(s) to be sent
-            s.send(bytes(json_to_send, 'utf-8') + (b'\0' *  (BUFFER_SIZE - len(json_to_send) )))
+      # os.listdir("files")
+      #filename = input()
 
-            # receive the server "yes send" or "no, already have the file" message
-            go_ahead = s.recv(1)
-            print(go_ahead)
+      # r - open for reading, b - binary mode
+      f = open(DIR + filename, "rb")
+      # s.send(bytes(filename, 'utf-8') + (b'\0' *  (64 - len(filename) ))) # send over the filename json so the server knows what we want to send
+      json_to_send = utils.jsonify(DIR + filename)
+      # max json data size of 1024 bytes
+      # send the server the json data of the file(s) to be sent
+      s.send(bytes(json_to_send, 'utf-8') + (b'\0' * (BUFFER_SIZE - len(json_to_send))))
 
-            if go_ahead == b'y': # yes send
-                print("inside yes")
-                # send the file
-                l = f.read(BUFFER_SIZE)
-                while l:
-                    s.send(l)
-                    l = f.read(BUFFER_SIZE)
-                    print("Sending:", l)
-                    print(type(l))
-                    print(not not l)
-            else:
-                print("[CLIENT]: Server said destination already contains a file with that name and checksum")
-                # else don't send the file
-                pass
+      # receive the server "yes send" or "no, already have the file" message
+      go_ahead = s.recv(1)
+      print(go_ahead)
+
+      if go_ahead == b'y':  # yes send
+        print("inside yes")
+        # send the file
+        l = f.read(BUFFER_SIZE)
+        while l:
+          s.send(l)
+          l = f.read(BUFFER_SIZE)
+          print("Sending:", l)
+          print(type(l))
+          print(not not l)
+      else:
+        print("[CLIENT]: Server said destination already contains a file with that name and checksum")
+        # else don't send the file
+        pass
 
 
 if __name__ == "__main__":
-    program = ClientApp()
-    program.mainloop()
+  program = ClientApp()
+  program.mainloop()
--- server.py
+++ server.py
@@ -12,33 +12,37 @@ TCP_PORT = 5003
 BUFFER_SIZE = 1024
 DIR = "dest/"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+def serv_loop():
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((TCP_IP, TCP_PORT))
     s.listen(1)
 
     # main loop
     while True:
-        conn, addr = s.accept()
-        print("Connection address", addr)
-        json_file_data = json.loads(conn.recv(BUFFER_SIZE).rstrip(b'\0').decode('utf-8')) # get the json file data from the client and convert it to string
-        print(json_file_data)
+      conn, addr = s.accept()
+      print("Connection address", addr)
+      json_file_data = json.loads(conn.recv(BUFFER_SIZE).rstrip(b'\0').decode('utf-8')) # get the json file data from the client and convert it to string
+      print(json_file_data)
 
-        if not os.path.isdir(DIR):
-            os.mkdir(DIR)
+      if not os.path.isdir(DIR):
+        os.mkdir(DIR)
 
-        if (os.path.isfile(DIR + json_file_data["filename"]) and json_file_data["md5checksum"] != utils.md5(DIR + json_file_data["filename"])) or not os.path.isfile(DIR + json_file_data["filename"]):
-            # TODO: send the 'go ahead' signal to the client to send the file
-            conn.send(b'y')
-            with open('dest/' + json_file_data["filename"], 'wb') as f: # w - open file for writing (truncates), b = binary mode
-                data = conn.recv(BUFFER_SIZE)
-                while data:
-                    print("Received:\nFilename: ", json_file_data["filename"])
-                    print("Received:\nData: ", data)
-                    f.write(data)
-                    data = conn.recv(BUFFER_SIZE)
-        else:
-            # file with that name exists and has the same checksum, no need to send
-            # TODO: create a list of error codes to send to the client
-            conn.send(b'n')
-            print("[SERVER]: Client tried to send duplicate filename \"%s\" with identical checksum \"%s\"" % (str(json_file_data["filename"]), str(json_file_data["md5checksum"])) )
-        conn.close()
+      if (os.path.isfile(DIR + json_file_data["filename"]) and json_file_data["md5checksum"] != utils.md5(DIR + json_file_data["filename"])) or not os.path.isfile(DIR + json_file_data["filename"]):
+        # TODO: send the 'go ahead' signal to the client to send the file
+        conn.send(b'y')
+        with open('dest/' + json_file_data["filename"], 'wb') as f: # w - open file for writing (truncates), b = binary mode
+          data = conn.recv(BUFFER_SIZE)
+          while data:
+            print("Received:\nFilename: ", json_file_data["filename"])
+            print("Received:\nData: ", data)
+            f.write(data)
+            data = conn.recv(BUFFER_SIZE)
+      else:
+        # file with that name exists and has the same checksum, no need to send
+        # TODO: create a list of error codes to send to the client
+        conn.send(b'n')
+        print("[SERVER]: Client tried to send duplicate filename \"%s\" with identical checksum \"%s\"" % (str(json_file_data["filename"]), str(json_file_data["md5checksum"])) )
+      conn.close()
+
+if __name__ == "__main__":
+    serv_loop()
